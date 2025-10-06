ab5e6fd252ad07e6bd760252493a057fade1b61c
Dayki <mavder76@gmail.com>
2025-10-07 00:53:25 +0500

    alright

--- tasks/task2.py
+++ tasks/task2.py
@@ -4,8 +4,10 @@ def solve():
 # Ниже пишите решение задачи
 
    a=int(input())
-   if a>=1440:
+   if a>=2400:
       print(23, 59)
+   elif a>1400:
+      print(a//60-24, a%60)
    else:
       print(a//60, a%60)
 
